fe9947f528cceceec2aceeb78943d3dca0f5c648
EmmanuelMess <emmanuelbendavid@gmail.com>
2020-07-28 14:50:15 -0300

    Fix indentation and formatting

--- test.c
+++ test.c
@@ -32,30 +32,24 @@ int genloglen;
 int genloglines;
 int firstnewline = -1;
 
-void example_log_function( int readSize, char * buf )
-{
-	static og_mutex_t * mt;
-	if( !mt ) mt = OGCreateMutex();
-	OGLockMutex( mt );
+void example_log_function( int readSize, char * buf ) {
+	static og_mutex_t *mt;
+	if (!mt) mt = OGCreateMutex();
+	OGLockMutex(mt);
 	int i;
-	for( i = 0; (readSize>=0)?(i <= readSize):buf[i]; i++ )
-	{
+	for (i = 0; (readSize >= 0) ? (i <= readSize) : buf[i]; i++) {
 		char c = buf[i];
-		if( c == '\0' ) c = '\n';
-		if( ( c != '\n' && genlinelen >= GENLINEWIDTH ) || c == '\n' )
-		{
+		if (c == '\0') c = '\n';
+		if ((c != '\n' && genlinelen >= GENLINEWIDTH) || c == '\n') {
 			int k;
 			genloglines++;
-			if( genloglines >= GENLINES )
-			{
-				genloglen -= firstnewline+1;
+			if (genloglines >= GENLINES) {
+				genloglen -= firstnewline + 1;
 				int offset = firstnewline;
 				firstnewline = -1;
 
-				for( k = 0; k < genloglen; k++ )
-				{
-					if( ( genlog[k] = genlog[k+offset+1] ) == '\n' && firstnewline < 0)
-					{
+				for (k = 0; k < genloglen; k++) {
+					if ((genlog[k] = genlog[k + offset + 1]) == '\n' && firstnewline < 0) {
 						firstnewline = k;
 					}
 				}
@@ -63,19 +57,18 @@ void example_log_function( int readSize, char * buf )
 				genloglines--;
 			}
 			genlinelen = 0;
-			if( c != '\n' )
-			{
-				genlog[genloglen+1] = 0;
+			if (c != '\n') {
+				genlog[genloglen + 1] = 0;
 				genlog[genloglen++] = '\n';
 			}
-			if( firstnewline < 0 ) firstnewline = genloglen;
+			if (firstnewline < 0) firstnewline = genloglen;
 		}
-		genlog[genloglen+1] = 0;
+		genlog[genloglen + 1] = 0;
 		genlog[genloglen++] = c;
-		if( c != '\n' ) genlinelen++;
+		if (c != '\n') genlinelen++;
 	}
 
-	OGUnlockMutex( mt );
+	OGUnlockMutex(mt);
 }
 
 volatile int suspended;
@@ -91,22 +84,21 @@ int lastkey, lastkeydown;
 
 static int keyboard_up;
 
-void HandleKey( int keycode, int bDown )
-{
+void HandleKey( int keycode, int bDown ) {
 	lastkey = keycode;
 	lastkeydown = bDown;
-	if( keycode == 4 ) { AndroidSendToBack( 1 ); }
+	if (keycode == 4) {
+		AndroidSendToBack(1);
+	}
 }
 
-void HandleButton( int x, int y, int button, int bDown )
-{
+void HandleButton( int x, int y, int button, int bDown ) {
 	lastbid = button;
 	lastbuttonx = x;
 	lastbuttony = y;
 }
 
-void HandleMotion( int x, int y, int mask )
-{
+void HandleMotion( int x, int y, int mask ) {
 	lastmask = mask;
 	lastmotionx = x;
 	lastmotiony = y;
@@ -115,22 +107,18 @@ void HandleMotion( int x, int y, int mask )
 extern struct android_app * gapp;
 
 
-void HandleDestroy()
-{
+void HandleDestroy() {
 }
 
-void HandleSuspend()
-{
+void HandleSuspend() {
 	suspended = 1;
 }
 
-void HandleResume()
-{
+void HandleResume() {
 	suspended = 0;
 }
 
-int main()
-{
+int main() {
 	int i, x, y;
 	double ThisTime;
 	double LastFPSTime = OGGetAbsoluteTime();
@@ -140,21 +128,19 @@ int main()
 
 	CNFGBGColor = 0x400000;
 	CNFGDialogColor = 0x444444;
-	CNFGSetupFullscreen( "Test Bench", 0 );
+	CNFGSetupFullscreen("Test Bench", 0);
 
-	const char * assettext = "Not Found";
-	AAsset * file = AAssetManager_open( gapp->activity->assetManager, "asset.txt", AASSET_MODE_BUFFER );
-	if( file )
-	{
+	const char *assettext = "Not Found";
+	AAsset *file = AAssetManager_open(gapp->activity->assetManager, "asset.txt", AASSET_MODE_BUFFER);
+	if (file) {
 		size_t fileLength = AAsset_getLength(file);
-		char * temp = malloc( fileLength + 1);
-		memcpy( temp, AAsset_getBuffer( file ), fileLength );
+		char *temp = malloc(fileLength + 1);
+		memcpy(temp, AAsset_getBuffer(file), fileLength);
 		temp[fileLength] = 0;
 		assettext = temp;
 	}
 
-	while(1)
-	{
+	while (1) {
 		int i, pos;
 		float f;
 		iframeno++;
@@ -162,43 +148,48 @@ int main()
 
 		CNFGHandleInput();
 
-		if( suspended ) { usleep(50000); continue; }
+		if (suspended) {
+			usleep(50000);
+			continue;
+		}
 
 		CNFGClearFrame();
-		CNFGColor( 0xFFFFFF );
-		CNFGGetDimensions( &screenx, &screeny );
+		CNFGColor(0xFFFFFF);
+		CNFGGetDimensions(&screenx, &screeny);
 
 		// Mesh in background
-		CNFGColor( 0xffffff );
-		CNFGPenX = 20; CNFGPenY = 20;
-		CNFGDrawText( assettext, 10 );
+		CNFGColor(0xffffff);
+		CNFGPenX = 20;
+		CNFGPenY = 20;
+		CNFGDrawText(assettext, 10);
 		CNFGFlushRender();
 
-		CNFGPenX = 0; CNFGPenY = 480;
+		CNFGPenX = 0;
+		CNFGPenY = 480;
 		char st[50];
-		sprintf( st, "%dx%d %d %d %d %d %d %d\n%d %d", screenx, screeny, lastbuttonx, lastbuttony, lastmotionx, lastmotiony, lastkey, lastkeydown, lastbid, lastmask );
-		CNFGDrawText( st, 10 );
-		glLineWidth( 2.0 );
+		sprintf(st, "%dx%d %d %d %d %d %d %d\n%d %d", screenx, screeny, lastbuttonx, lastbuttony, lastmotionx,
+		        lastmotiony, lastkey, lastkeydown, lastbid, lastmask);
+		CNFGDrawText(st, 10);
+		glLineWidth(2.0);
 
 		// Square behind text
-		CNFGDrawBox( 600, 0, 950, 350);
+		CNFGDrawBox(600, 0, 950, 350);
 
-		CNFGPenX = 10; CNFGPenY = 10;
+		CNFGPenX = 10;
+		CNFGPenY = 10;
 
 		// Text
 		pos = 0;
-		CNFGColor( 0xffffff );
-		for( i = 0; i < 1; i++ )
-		{
+		CNFGColor(0xffffff);
+		for (i = 0; i < 1; i++) {
 			int c;
-			char tw[2] = { 0, 0 };
-			for( c = 0; c < 256; c++ )
-			{
+			char tw[2] = {0, 0};
+			for (c = 0; c < 256; c++) {
 				tw[0] = c;
 
-				CNFGPenX = ( c % 16 ) * 20+606;
-				CNFGPenY = ( c / 16 ) * 20+5;
-				CNFGDrawText( tw, 4 );
+				CNFGPenX = (c % 16) * 20 + 606;
+				CNFGPenY = (c / 16) * 20 + 5;
+				CNFGDrawText(tw, 4);
 			}
 		}
 
@@ -206,36 +197,33 @@ int main()
 		CNFGPenX = 0;
 		CNFGPenY = 0;
 
-		for( i = 0; i < 400; i++ )
-		{
+		for (i = 0; i < 400; i++) {
 			RDPoint pp[3];
-			CNFGColor( 0x00FF00 );
-			pp[0].x = (short)(50*sin((float)(i+iframeno)*.01) + (i%20)*30);
-			pp[0].y = (short)(50*cos((float)(i+iframeno)*.01) + (i/20)*20);
-			pp[1].x = (short)(20*sin((float)(i+iframeno)*.01) + (i%20)*30);
-			pp[1].y = (short)(50*cos((float)(i+iframeno)*.01) + (i/20)*20);
-			pp[2].x = (short)(10*sin((float)(i+iframeno)*.01) + (i%20)*30);
-			pp[2].y = (short)(30*cos((float)(i+iframeno)*.01) + (i/20)*20);
-			CNFGTackPoly( pp, 3 );
+			CNFGColor(0x00FF00);
+			pp[0].x = (short) (50 * sin((float) (i + iframeno) * .01) + (i % 20) * 30);
+			pp[0].y = (short) (50 * cos((float) (i + iframeno) * .01) + (i / 20) * 20);
+			pp[1].x = (short) (20 * sin((float) (i + iframeno) * .01) + (i % 20) * 30);
+			pp[1].y = (short) (50 * cos((float) (i + iframeno) * .01) + (i / 20) * 20);
+			pp[2].x = (short) (10 * sin((float) (i + iframeno) * .01) + (i % 20) * 30);
+			pp[2].y = (short) (30 * cos((float) (i + iframeno) * .01) + (i / 20) * 20);
+			CNFGTackPoly(pp, 3);
 		}
 
 		CNFGPenX = 5;
 		CNFGPenY = 600;
-		CNFGDrawText( genlog, 4 );
+		CNFGDrawText(genlog, 4);
 
 		frames++;
 		CNFGSwapBuffers();
 
 		ThisTime = OGGetAbsoluteTime();
-		if( ThisTime > LastFPSTime + 1 )
-		{
-			printf( "FPS: %d\n", frames );
+		if (ThisTime > LastFPSTime + 1) {
+			printf("FPS: %d\n", frames);
 			frames = 0;
 			linesegs = 0;
-			LastFPSTime+=1;
+			LastFPSTime += 1;
 		}
 	}
 
-	return(0);
+	return (0);
 }
-
